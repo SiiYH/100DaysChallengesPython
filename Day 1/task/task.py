47c3f896baa7e332be389a01f8386d578e14527c
SiiYH <syewhei@gmail.com>
2026-03-11 07:51:25 +0800

    merge origin main

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -9,3 +9,6 @@ city = input("What's the name of the city you grew up in? \n")
 pet = input("What's your pet's name? \n")
 
 print("Your band name could be " + city + " " + pet)
+
+print("My name is" + " " + "Angela")
+
